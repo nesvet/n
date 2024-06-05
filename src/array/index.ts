49d5acdf1502dde2493cd1477e8388a2db9ebbf4
Eugene Nesvetaev <dev@nesvet.dev>
2024-06-05 14:51:41 +0400

    feat: randomItem

--- src/array/index.ts
+++ src/array/index.ts
@@ -1,6 +1,7 @@
 export * from "./add.js";
 export * from "./chunks.js";
 export * from "./intersection.js";
+export * from "./randomItem.js";
 export * from "./removeAll.js";
 export * from "./removeOne.js";
 export * from "./unique.js";
---
+++ src/array/randomItem.ts
@@ -0,0 +1,6 @@
+import { random } from "../random.js";
+
+
+export function randomItem<Item>(array: Item[]) {
+	return array[random(0, array.length - 1)];
+}
