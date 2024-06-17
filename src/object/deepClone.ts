4996fdd2534d4a62a2a814997f2d10890894f2d3
Eugene Nesvetaev <dev@nesvet.dev>
2024-06-17 14:34:45 +0400

    chore: Deprecated deepClone in favour of structuredClone

--- src/object/deepClone.ts
+++ src/object/deepClone.ts
@@ -1,3 +1,4 @@
+/** @deprecated */
 export function deepClone(object: object) {
-	return JSON.parse(JSON.stringify(object));
+	return JSON.parse(JSON.stringify(object));// eslint-disable-line unicorn/prefer-structured-clone
 }
