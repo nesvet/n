2a5201bc814b29bf3430fda7b3ba0010771af2de
Eugene Nesvetaev <dev@nesvet.dev>
2024-08-01 14:31:07 +0400

    fix: Typings

--- src/map/getAll.ts
+++ src/map/getAll.ts
@@ -1,3 +1,5 @@
+export function getAll<K, V>(map: Map<K, V>, keys: K[], onlyExisting?: false): (undefined | V)[];
+export function getAll<K, V>(map: Map<K, V>, keys: K[], onlyExisting: true): V[];
 export function getAll<K, V>(map: Map<K, V>, keys: K[], onlyExisting = false) {
 	if (onlyExisting)
 		keys = keys.filter(key => map.has(key));
