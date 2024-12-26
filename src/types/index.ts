5c0085a9cc4bcd80e84ceea22ab680cf4e8a6457
Eugene Nesvetaev <dev@nesvet.dev>
2024-12-26 14:23:13 +0400

    feat: Mutable utility type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,6 @@ export * from "./AnyProp.js";
 export * from "./ExtendsOrOmit.js";
 export * from "./Falsy.js";
 export * from "./isTruthy.js";
+export * from "./Mutable.js";
 export * from "./StringKey.js";
 export * from "./Truthy.js";
