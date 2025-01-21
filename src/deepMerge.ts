d89e21ebf12ca88d99dca3dac06dce3d876bad31
Eugene Nesvetaev <dev@nesvet.dev>
2025-01-21 13:54:23 +0400

    fix: deepMerge tuned

--- src/deepMerge.ts
+++ src/deepMerge.ts
@@ -2,8 +2,9 @@ import { flat } from "./flat.js";
 import { unflat } from "./unflat.js";
 
 
-export function deepMerge(target: Record<string, unknown>, ...args: (boolean | Record<string, unknown>)[]) {
+export function deepMerge(...args: (boolean | Record<string, unknown>)[]) {
 	const strict = Boolean(typeof args.at(-1) == "boolean" ? args.pop() : true);
+	const target = args.shift() as Record<string, unknown>;
 	
 	return unflat(
 		Object.assign(
