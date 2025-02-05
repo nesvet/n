2a3f5c49c5f956085b86e9f31622d34d4ff7d7c7
Eugene Nesvetaev <dev@nesvet.dev>
2025-02-05 15:25:43 +0400

    feat: intersection supports empty args

--- src/intersection.ts
+++ src/intersection.ts
@@ -1,12 +1,16 @@
-export function intersection<I>(firstIterable: I[] | Set<I>, ...otherIterables: (I[] | Set<I>)[]): I[] {
+export function intersection<I>(...iterables: (I[] | Set<I>)[]): I[] {
 	
 	const intersectionArray = [];
 	
-	mainLoop: for (const item of firstIterable) {
-		for (const otherIterable of otherIterables)
-			if (otherIterable instanceof Set ? !otherIterable.has(item) : !otherIterable.includes(item))
-				continue mainLoop;
-		intersectionArray.push(item);
+	if (iterables.length) {
+		const [ firstIterable, ...otherIterables ] = iterables;
+		
+		mainLoop: for (const item of firstIterable) {
+			for (const otherIterable of otherIterables)
+				if (otherIterable instanceof Set ? !otherIterable.has(item) : !otherIterable.includes(item))
+					continue mainLoop;
+			intersectionArray.push(item);
+		}
 	}
 	
 	return intersectionArray;
