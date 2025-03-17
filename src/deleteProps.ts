fba184afc25521d0075f232a3277dec320aeb76b
Eugene Nesvetaev <dev@nesvet.dev>
2025-03-17 16:18:49 +0400

    chore: Typings

--- src/deleteProps.ts
+++ src/deleteProps.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
+export function deleteProps(object: null | undefined, keys: any): null;
+export function deleteProps<O extends object, K extends readonly (keyof O | number | string | symbol)[]>(object: O, keys: K): Omit<O, K[number]>;
 export function deleteProps<O extends object, K extends readonly (keyof O | number | string | symbol)[]>(object: O, keys: K) {
 	if (object) {
 		for (const key of keys)
