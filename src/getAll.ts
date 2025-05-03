6be8f320999455d900dfae1bab702a2f6fefa5a1
Eugene Nesvetaev <dev@nesvet.dev>
2025-05-03 14:25:12 +0400

    refactor: getAll

--- src/getAll.ts
+++ src/getAll.ts
@@ -1,8 +1,21 @@
-export function getAll<K, V>(map: Map<K, V>, keys: K[], onlyExisting?: false): (undefined | V)[];
-export function getAll<K, V>(map: Map<K, V>, keys: K[], onlyExisting: true): V[];
-export function getAll<K, V>(map: Map<K, V>, keys: K[], onlyExisting = false) {
-	if (onlyExisting)
-		keys = keys.filter(key => map.has(key));
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
+export function getAll<V>(map: Map<any, V>, keys: any[], onlyExisting: true): V[];
+export function getAll<V>(map: Map<any, V>, keys: any[], onlyExisting?: false): (V | undefined)[];
+export function getAll<V>(map: Map<any, V>, keys: any[], onlyExisting = false) {
 	
-	return keys.map(key => map.get(key));
+	const items = [];
+	
+	if (onlyExisting)
+		for (const key of keys) {
+			const item = map.get(key);
+			if (item !== undefined)
+				items.push(item);
+		}
+	else
+		for (const key of keys)
+			items.push(map.get(key));
+	
+	return items;
 }
