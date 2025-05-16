3484c59632f4ac1ee2601af2cb3a6dd3e679baa1
Eugene Nesvetaev <dev@nesvet.dev>
2025-05-16 15:09:09 +0400

    feat: handleMongoError enhanced

--- src/handleMongoError.ts
+++ src/handleMongoError.ts
@@ -11,25 +11,28 @@ type MongoServerError = {
 			[key: string]: any;
 		};
 	};
+	isMongoServerError?: true;
 	[key: string]: any;
 };
 
-function isMongoServerError(error: unknown): error is MongoServerError {
-	return (
-		!!error &&
-		typeof error == "object" &&
-		"errorResponse" in error &&
-		!!error.errorResponse &&
-		typeof error.errorResponse == "object" &&
-		"errInfo" in error.errorResponse
-	);
+function makeMessageFromDocumentFailedValidationErrorResponse(errorResponse: MongoServerError["errorResponse"]) {
+	return `${indent(`failingDocumentId: ${errorResponse.errInfo?.failingDocumentId}\nschemaRulesNotSatisfied: ${JSON.stringify(errorResponse.errInfo?.details?.schemaRulesNotSatisfied, null, "  ")}`, "  ")}`;
 }
 
 export function handleMongoError<E extends MongoServerError>(error: E): E;
 export function handleMongoError(error: any): any;
 export function handleMongoError(error: any) {
-	if (isMongoServerError(error) && error.errorResponse.errmsg === "Document failed validation")
-		error.message = `${error.errorResponse.errmsg}\n${indent(`failingDocumentId: ${error.errorResponse.errInfo?.failingDocumentId}\nschemaRulesNotSatisfied: ${JSON.stringify(error.errorResponse.errInfo?.details?.schemaRulesNotSatisfied, null, "  ")}`, "  ")}`;
+	
+	try {
+		const documentFailedValidationResponses =
+			("writeErrors" in error ? error.writeErrors : [ error.errorResponse ])
+				.filter((errorResponse: MongoServerError["errorResponse"]) => errorResponse.errmsg === "Document failed validation");
+		
+		if (documentFailedValidationResponses.length)
+			error.message += `\n${documentFailedValidationResponses.map(makeMessageFromDocumentFailedValidationErrorResponse).join("\n")}`;
+		
+		error.isMongoServerError = true;
+	} catch {}
 	
 	return error;
 }
